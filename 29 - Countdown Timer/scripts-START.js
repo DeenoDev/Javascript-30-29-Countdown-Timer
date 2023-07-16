34e26eeebd585eb48160db36ef4128c3214d6229
DeenoDev <deyanoklaasen@outlook.com>
2023-07-16 15:54:00 +0200

    displayEndTime functioncompleted and merged with timer function.

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,11 +1,13 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 
 function timer(seconds){
    const now = Date.now();
    const then = now + seconds * 1000;
    displayTimeLeft(seconds);
+   displayEndTime(then);
 
    countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -27,4 +29,13 @@ function displayTimeLeft(seconds){
     document.title = display;
     timerDisplay.textContent = display;
     console.log({minutes, remainderSeconds});
+}
+
+function displayEndTime(timestamp){
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+
 }
