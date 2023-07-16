b9a1da7d2ae32e08ecb2d46610a5134e96cfc4a8
DeenoDev <deyanoklaasen@outlook.com>
2023-07-16 16:08:23 +0200

    startTimer function completed and used clearInterval to stop multiple timers from running. startTimer also linked to eventlistener.

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -5,6 +5,9 @@ const buttons = document.querySelectorAll('[data-time]');
 
 
 function timer(seconds){
+    //clear any existing timers
+    clearInterval(countdown);
+    
    const now = Date.now();
    const then = now + seconds * 1000;
    displayTimeLeft(seconds);
