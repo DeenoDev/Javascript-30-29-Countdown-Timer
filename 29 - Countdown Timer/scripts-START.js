8d4b90d2ba350af895f82556b8c685a698f9aff8
DeenoDev <deyanoklaasen@outlook.com>
2023-07-16 15:29:31 +0200

    displayTimeLeft function created.

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,5 +1,27 @@
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+
+
 function timer(seconds){
-   const now = date.now();
+   const now = Date.now();
    const then = now + seconds * 1000;
-   console.log({now, then});
+   displayTimeLeft(seconds);
+
+   countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    //check if we should stop it!
+    if(secondsLeft <= 0){
+        clearInterval(countdown);
+        return;
+    }
+    //display it
+    displayTimeLeft(secondsLeft);
+
+   }, 1000);
+}
+
+function displayTimeLeft(seconds){
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    console.log({minutes, remainderSeconds});
 }
