aa43e8a16ad9babdba63ca3c42b0649c5a3580ee
DeenoDev <deyanoklaasen@outlook.com>
2023-07-16 15:37:08 +0200

    displayTimeLeft function completed and displayEndTime created.

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -23,5 +23,8 @@ function timer(seconds){
 function displayTimeLeft(seconds){
     const minutes = Math.floor(seconds / 60);
     const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds  < 10 ? '0' : ''}${remainderSeconds}`;
+    document.title = display;
+    timerDisplay.textContent = display;
     console.log({minutes, remainderSeconds});
 }
